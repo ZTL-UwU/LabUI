d0d7fc0e1b287d4ca150479b971cb724511ba227
zhangtianli2006 <zhangtianli2006@163.com>
2021-05-04 16:34:30 +0800

    [feature] Plain Button
    Signed-off-by: zhangtianli2006 <zhangtianli2006@163.com>

--- public/demo/App.svelte
+++ public/demo/App.svelte
@@ -3,47 +3,58 @@
     import * as colors from '../../src/color.ts';
 </script>
 
-<div class="item-20">
-    <LButton>Demo</LButton>
-    <LButton disabled>Disabled</LButton>
-</div>
+<main>
+    <div class="item-20">
+        <LButton>Demo</LButton>
+        <LButton disabled>Disabled</LButton>
+        <LButton>
+            <img src="https://avatars.githubusercontent.com/u/49156174?s=45&v=4" />
+        </LButton>
+    </div>
 
-<div class="item-15">
-    <LButton size="small">Small</LButton>
-    <LButton>Normal</LButton>
-    <LButton size="large">Large</LButton>
-    <LButton height="80px" width="200px">Custom</LButton>
-</div>
+    <div class="item-15">
+        <LButton size="small">Small</LButton>
+        <LButton>Normal</LButton>
+        <LButton size="large">Large</LButton>
+        <LButton height="80px" width="200px">Custom</LButton>
+    </div>
 
-<div class="item-20">
-    <LButton disabled size="small">Small</LButton>
-    <LButton disabled>Normal</LButton>
-    <LButton disabled size="large">Large</LButton>
-    <LButton disabled height="80px" width="200px">Custom</LButton>
-</div>
+    <div class="item-20">
+        <LButton disabled size="small">Small</LButton>
+        <LButton disabled>Normal</LButton>
+        <LButton disabled size="large">Large</LButton>
+        <LButton disabled height="80px" width="200px">Custom</LButton>
+    </div>
 
-<div class="item-20">
-    <LButton color="blue">Blue</LButton>
-    <LButton color="green">Green</LButton>
-    <LButton color="yellow">Yellow</LButton>
-    <LButton color="red">Red</LButton>
-    <LButton color="grey">Grey</LButton>
-</div>
+    <div class="item-20">
+        <LButton color="blue">Blue</LButton>
+        <LButton color="green">Green</LButton>
+        <LButton color="yellow">Yellow</LButton>
+        <LButton color="red">Red</LButton>
+        <LButton color="grey" size="large">Grey Large</LButton>
+    </div>
 
-<div class="item-20">
-    <LProgress size="small" color={colors.red} percent="10"></LProgress>
-    <LProgress color={colors.green} percent="100" width="300px"></LProgress>
-    <LProgress size="large"></LProgress>
-</div>
+    <div class="item-20">
+        <LButton plain color="blue">Plain</LButton>
+        <LButton plain color="green" size="large">large</LButton>
+        <LButton plain disabled>Plain Disabled</LButton>
+    </div>
 
-<div class="item-20">
-    <LMessageBar>
-        <span slot="content">Message</span>
-    </LMessageBar>
-    <LMessageBar type="warning">
-        <span slot="content">Warning</span>
-    </LMessageBar>
-</div>
+    <div class="item-20">
+        <LProgress size="small" color={colors.red} percent="10"></LProgress>
+        <LProgress color={colors.green} percent="100" width="300px"></LProgress>
+        <LProgress size="large"></LProgress>
+    </div>
+
+    <div class="item-20">
+        <LMessageBar>
+            <span slot="content">Message</span>
+        </LMessageBar>
+        <LMessageBar type="warning">
+            <span slot="content">Warning</span>
+        </LMessageBar>
+    </div>
+</main>
 
 <style lang="scss">
     .item-20 {
--- src/Button.svelte
+++ src/Button.svelte
@@ -1,11 +1,14 @@
 <script lang="typescript">
     export let disabled = false;
+    export let plain = false;
     export let color = disabled ? '' : 'green';
     export let size = 'normal';
     export let height = '';
     export let width = '';
 
-    $: classes = `button button-${size} button-${color}`;
+    $: button_plain = plain ? 'button-plain' : '';
+    
+    $: classes = `button button-${size} button-${color} ${button_plain}`;
     let height_style: string = height === '' ? '' : `height: ${height};`
     let width_style: string = width === '' ? '' : `width: ${width};`;
     $: styles = height_style + width_style;
@@ -24,11 +27,23 @@
 
     .button {
         font-weight: 800;
+        padding: 8px 5px 8px 5px;
 
-        border: none;
         border-radius: $border-radius-normal;
-
+        
+        border: none;
         color: $white;
+        &.button-plain {
+            color: $grey7;
+            background-color: $transparent !important;
+
+            &:disabled { border: 3px solid $grey2; }
+            &.button-blue { border: 3px solid $blue2; }
+            &.button-green { border: 3px solid $green2; }
+            &.button-yellow { border: 3px solid $yellow1; }
+            &.button-grey { border: 3px solid $grey1; }
+            &.button-red { border: 3px solid $red2; }
+        }
         &.button-blue { background-color: $blue; }
         &.button-green { background-color: $green; }
         &.button-yellow { background-color: $yellow; }
@@ -38,6 +53,13 @@
         &.button-small {
             min-height: 24px;
             min-width: 80px;
+            &.button-plain {
+                &.button-blue { @include button-shadow(4px, $blue3); }
+                &.button-green { @include button-shadow(4px, $green3); }
+                &.button-yellow { @include button-shadow(4px, $yellow2); }
+                &.button-grey { @include button-shadow(4px, $grey2); }
+                &.button-red { @include button-shadow(4px, $red3); }
+            }
             &.button-blue { @include button-shadow(6px, $blue7); }
             &.button-green { @include button-shadow(6px, $green7); }
             &.button-yellow { @include button-shadow(6px, $yellow6); }
@@ -47,6 +69,13 @@
         &.button-normal {
             min-height: 30px;
             min-width: 100px;
+            &.button-plain {
+                &.button-blue { @include button-shadow(6px, $blue3); }
+                &.button-green { @include button-shadow(6px, $green3); }
+                &.button-yellow { @include button-shadow(6px, $yellow2); }
+                &.button-grey { @include button-shadow(6px, $grey2); }
+                &.button-red { @include button-shadow(6px, $red3); }
+            }
             &.button-green { @include button-shadow(8px, $green7); }
             &.button-blue { @include button-shadow(8px, $blue7); }
             &.button-yellow { @include button-shadow(8px, $yellow6); }
@@ -56,6 +85,13 @@
         &.button-large {
             min-height: 40px;
             min-width: 120px;
+            &.button-plain {
+                &.button-blue { @include button-shadow(8px, $blue3); }
+                &.button-green { @include button-shadow(8px, $green3); }
+                &.button-yellow { @include button-shadow(8px, $yellow2); }
+                &.button-grey { @include button-shadow(8px, $grey2); }
+                &.button-red { @include button-shadow(8px, $red3); }
+            }
             &.button-blue { @include button-shadow(10px, $blue7); }
             &.button-green { @include button-shadow(10px, $green7); }
             &.button-yellow { @include button-shadow(10px, $yellow6); }
@@ -66,6 +102,14 @@
         cursor: pointer;
 
         &:hover {
+            &.button-plain {
+                &:disabled { border: 3px solid $grey2; }
+                &.button-blue { border: 3px solid $blue3; }
+                &.button-green { border: 3px solid $green3; }
+                &.button-yellow { border: 3px solid $yellow2; }
+                &.button-grey { border: 3px solid $grey2; }
+                &.button-red { border: 3px solid $red3; }
+            }
             &.button-blue { background-color: $blue7; }
             &.button-green { background-color: $green7; }
             &.button-yellow { background-color: $yellow6; }
@@ -73,6 +117,13 @@
             &.button-red { background-color: $red7; }
 
             &.button-small {
+                &.button-plain {
+                    &.button-blue { @include button-shadow(4px, $blue4); }
+                    &.button-green { @include button-shadow(4px, $green4); }
+                    &.button-yellow { @include button-shadow(4px, $yellow3); }
+                    &.button-grey { @include button-shadow(4px, $grey3); }
+                    &.button-red { @include button-shadow(4px, $red4); }
+                }
                 &.button-blue { @include button-shadow(6px, $blue8); }
                 &.button-green { @include button-shadow(6px, $green8); }
                 &.button-yellow { @include button-shadow(6px, $yellow7); }
@@ -80,6 +131,13 @@
                 &.button-red { @include button-shadow(6px, $red8); }
             }
             &.button-normal {
+                &.button-plain {
+                    &.button-blue { @include button-shadow(6px, $blue4); }
+                    &.button-green { @include button-shadow(6px, $green4); }
+                    &.button-yellow { @include button-shadow(6px, $yellow3); }
+                    &.button-grey { @include button-shadow(6px, $grey3); }
+                    &.button-red { @include button-shadow(6px, $red4); }
+                }
                 &.button-blue { @include button-shadow(8px, $blue8); }
                 &.button-green { @include button-shadow(8px, $green8); }
                 &.button-yellow { @include button-shadow(8px, $yellow7); }
@@ -87,6 +145,13 @@
                 &.button-red { @include button-shadow(8px, $red8); }
             }
             &.button-large {
+                &.button-plain {
+                    &.button-blue { @include button-shadow(8px, $blue4); }
+                    &.button-green { @include button-shadow(8px, $green4); }
+                    &.button-yellow { @include button-shadow(8px, $yellow3); }
+                    &.button-grey { @include button-shadow(8px, $grey3); }
+                    &.button-red { @include button-shadow(8px, $red4); }
+                }
                 &.button-blue { @include button-shadow(10px, $blue8); }
                 &.button-green { @include button-shadow(10px, $green8); }
                 &.button-yellow { @include button-shadow(10px, $yellow7); }
@@ -97,7 +162,14 @@
 
         &:active {
             transform: translateY(2px);
-            &.button-small { 
+            &.button-small {
+                &.button-plain {
+                    &.button-blue { @include button-shadow(2px, $blue4); }
+                    &.button-green { @include button-shadow(2px, $green4); }
+                    &.button-yellow { @include button-shadow(2px, $yellow3); }
+                    &.button-grey { @include button-shadow(2px, $grey3); }
+                    &.button-red { @include button-shadow(2px, $red4); }
+                }
                 &.button-blue { @include button-shadow(4px, $blue8); }
                 &.button-green { @include button-shadow(4px, $green8); }
                 &.button-yellow { @include button-shadow(4px, $yellow7); }
@@ -105,6 +177,13 @@
                 &.button-red { @include button-shadow(4px, $red8); }
             }
             &.button-normal {
+                &.button-plain {
+                    &.button-blue { @include button-shadow(4px, $blue4); }
+                    &.button-green { @include button-shadow(4px, $green4); }
+                    &.button-yellow { @include button-shadow(4px, $yellow3); }
+                    &.button-grey { @include button-shadow(4px, $grey3); }
+                    &.button-red { @include button-shadow(4px, $red4); }
+                }
                 &.button-blue { @include button-shadow(6px, $blue8); }
                 &.button-green { @include button-shadow(6px, $green8); }
                 &.button-yellow { @include button-shadow(6px, $yellow7); }
@@ -112,6 +191,13 @@
                 &.button-red { @include button-shadow(6px, $red8); }
             }
             &.button-large {
+                &.button-plain {
+                    &.button-blue { @include button-shadow(6px, $blue4); }
+                    &.button-green { @include button-shadow(6px, $green4); }
+                    &.button-yellow { @include button-shadow(6px, $yellow3); }
+                    &.button-grey { @include button-shadow(6px, $grey3); }
+                    &.button-red { @include button-shadow(6px, $red4); }
+                }
                 &.button-blue { @include button-shadow(8px, $blue8); }
                 &.button-green { @include button-shadow(8px, $green8); }
                 &.button-yellow { @include button-shadow(8px, $yellow7); }
@@ -123,6 +209,11 @@
         &:disabled {
             background-color: $grey2;
 
+            &.button-plain {
+                border-color: $grey2;
+                @include button-shadow(6px, $grey3 !important);
+                &:hover { border-color: $grey2; }
+            }
             &.button-small { @include button-shadow(6px, $grey3); }
             &.button-normal { @include button-shadow(8px, $grey3); }
             &.button-large { @include button-shadow(10px, $grey3); }
