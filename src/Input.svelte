dfc133d702c67e46c5e21c77a5710800bae4ad4b
zhangtianli2006 <zhangtianli2006@163.com>
2021-05-05 16:49:29 +0800

    [feature] password Input show / hid
    Signed-off-by: zhangtianli2006 <zhangtianli2006@163.com>

--- src/Input.svelte
+++ src/Input.svelte
@@ -5,19 +5,33 @@
     export let type = 'text';
     export let limit = -1;
     export let flat = true;
-    
-    $: is_limited = limit == -1 ? false : true;
 
+    let show_pass_icon = type === 'password' ? true : false;
+    let show_password_icon = 'Show';
+
+    $: is_limited = limit == -1 ? false : true;
     $: counter = limit == -1 ? '' : `${value.length} / ${limit}`;
     $: limited_class = is_limited ? 'lb__input-limited' : '';
     $: show_3D_class = flat ? '' : 'lb__input-3D';
-    $: classes = `lb__input lb__input-${type} ${limited_class} ${show_3D_class}`;
+    $: password_class_mock = (show_pass_icon && type !== 'password') ? 'lb__input-password' : '';
+    $: classes = `lb__input lb__input-${type} ${password_class_mock} ${limited_class} ${show_3D_class}`;
 
     const handleInput = (event) => {
         if (!is_limited || event.target.value.length <= limit) {
             value = event.target.value;
         }
     };
+
+    function show_pass() {
+        if (type === 'text') {
+            type = 'password';
+            show_password_icon = 'Show';
+        } else if (type === 'password') {
+            type = 'text';
+            show_password_icon = 'Hide';
+        }
+        show_pass_icon = true;
+    }
 </script>
 
 {#if type!=='textarea'}
@@ -30,9 +44,15 @@
             {value}
             on:input={handleInput}
         />
-        {#if is_limited}
-            <span class="lb__input-counter">
-                <span class="lb__input-counter-inner">{counter}</span>
+
+        {#if is_limited || show_pass_icon}
+            <span class="lb__input-suffix">
+                <span class="lb__input-suffix-inner">
+                    {#if is_limited} {counter} {/if}
+                    {#if show_pass_icon}
+                        <span on:click={show_pass}>{show_password_icon}</span>
+                    {/if}
+                </span>
             </span>
         {/if}
     </div>
@@ -46,8 +66,8 @@
             on:input={handleInput}
         />
         {#if is_limited}
-            <span class="lb__input-counter lb__input-textarea-counter">
-                <span class="lb__input-counter-inner">{counter}</span>
+            <span class="lb__input-suffix lb__input-textarea-counter">
+                <span class="lb__input-suffix-inner">{counter}</span>
             </span>
         {/if}
     </div>
@@ -61,7 +81,7 @@
         display: inline-block;
     }
 
-    .lb__input-counter {
+    .lb__input-suffix {
         position: absolute;
 
         &.lb__input-textarea-counter {
@@ -106,7 +126,7 @@
     .lb__input-text, .lb__input-password, .lb__input-email {
         height: $height-normal;
 
-        &.lb__input-limited { padding-right: 45px; }
+        &.lb__input-limited, &.lb__input-password { padding-right: 45px; }
         padding: 2px 10px 2px 10px;
     }
 
