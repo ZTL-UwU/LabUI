c1322b54b2009f37f061aac63f24517b43e0168e
zhangtianli2006 <zhangtianli2006@163.com>
2021-06-03 13:33:27 +0800

    [feature] Mask on:click (Cover -> Mask)
    Signed-off-by: zhangtianli2006 <zhangtianli2006@163.com>

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Box          from './components/Box.svelte';
 import Button       from './components/Button.svelte';
-import Cover        from './components/Cover.svelte';
 import Input        from './components/Input.svelte';
 import Link         from './components/Link.svelte';
 import MDContainer  from './components/MDContainer.svelte';
 import Progress     from './components/Progress.svelte';
+import Mask         from './components/Mask.svelte';
 import MessageBar   from './components/MessageBar.svelte';
 import NumberPicker from './components/NumberPicker.svelte';
 import Radio        from './components/Radio.svelte';
@@ -13,9 +13,9 @@ import Switch       from './components/Switch.svelte';
 export {
     Box          as LBox,
     Button       as LButton,
-    Cover        as LCover,
     Input        as LInput,
     Link         as LLink,
+    Mask         as LMask,
     MDContainer  as LMDContainer,
     Progress     as LProgress,
     MessageBar   as LMessageBar,
