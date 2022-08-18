4af28c5673621b0f8322ab5b5b355101dd75bfdf
zhangtianli2006 <zhangtianli2006@163.com>
2022-08-18 11:08:21 +0800

    [Tags] colored tags
    Signed-off-by: zhangtianli2006 <zhangtianli2006@163.com>

--- src/components/Tags.svelte
+++ src/components/Tags.svelte
@@ -1,7 +1,14 @@
 <script>
+    import { mix_classes } from '../scripts/utils';
+
+    export let color = 'white3';
+    $: classes = mix_classes([
+        'tag-container',
+        `tag-${color}`,
+    ]);
 </script>
 
-<span class="lb__tag-container">
+<span class={classes}>
     <slot name="icon" />
     <slot name="text" />
 </span>
@@ -11,12 +18,23 @@
 
     .lb__tag-container {
         display: inline-block;
-        background: $white3;
         padding: 7px 13px 7px 13px;
         height: 17px;
         border-radius: $border-radius-large;
         font-weight: 500;
-        color: $grey;
         font-size: 14px;
+        margin: 2px;
+    }
+
+    @each $name, $val in $color-map {
+        .lb__tag-#{$name} {
+            background: $val;
+        }
+    }
+
+    @each $name, $val in $font-color-map {
+        .lb__tag-#{$name} {
+            color: $val;
+        }
     }
 </style>
